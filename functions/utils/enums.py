24a7f7cc55346f95a131e24bd9499397cb659ce2
Jesse Stover <j.stover.r@gmail.com>
2025-09-17 11:12:03 -0500

    Enums

---
+++ functions/utils/enums.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class CreateInput(Enum):
+    INPUT_1_CONTENT = "Test input1"
+    INPUT_2_CONTENT = "Test input2"
+    INPUT_3_CONTENT = "Test input3"
+
+
+class TestPyApi(Enum):
+    OUTPUT_1_CONTENT = "Test output1"
+    OUTPUT_2_CONTENT = "Test output2"
