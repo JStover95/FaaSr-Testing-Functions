ee6f47ced1f3efe3c28770e1f0e7a908b2e2c812
Jesse Stover <j.stover.r@gmail.com>
2025-09-19 12:41:32 -0500

    Added more verbose logging

--- functions/01_create_input.py
+++ functions/01_create_input.py
@@ -1,4 +1,4 @@
-from FaaSr_py.client.py_client_stubs import faasr_put_file
+from FaaSr_py.client.py_client_stubs import faasr_log, faasr_put_file
 
 from .utils import get_invocation_id
 from .utils.enums import CreateInput
@@ -11,31 +11,46 @@ def create_input(
     input3: str,
 ) -> None:
     invocation_id = get_invocation_id()
+    faasr_log(f"Using invocation ID: {invocation_id}")
 
     # Create input1
     with open(input1, "w") as f:
         f.write(CreateInput.INPUT_1_CONTENT.value)
 
+    remote_file = f"{invocation_id}/{input1}"
+    faasr_put_file(
+        local_file=input1,
+        remote_file=remote_file,
+        remote_folder=folder,
+    )
+    faasr_log(
+        f"Created input1: {remote_file} with content: {CreateInput.INPUT_1_CONTENT.value}"
+    )
+
     # Create input2
     with open(input2, "w") as f:
         f.write(CreateInput.INPUT_2_CONTENT.value)
+
+    remote_file = f"{invocation_id}/{input2}"
+    faasr_put_file(
+        local_file=input2,
+        remote_file=remote_file,
+        remote_folder=folder,
+    )
+    faasr_log(
+        f"Created input2: {remote_file} with content: {CreateInput.INPUT_2_CONTENT.value}"
+    )
 
     # Create input3
     with open(input3, "w") as f:
         f.write(CreateInput.INPUT_3_CONTENT.value)
 
-    faasr_put_file(
-        local_file=input1,
-        remote_file=f"{invocation_id}/{input1}",
-        remote_folder=folder,
-    )
-    faasr_put_file(
-        local_file=input2,
-        remote_file=f"{invocation_id}/{input2}",
-        remote_folder=folder,
-    )
+    remote_file = f"{invocation_id}/{input3}"
     faasr_put_file(
         local_file=input3,
-        remote_file=f"{invocation_id}/{input3}",
+        remote_file=remote_file,
         remote_folder=folder,
+    )
+    faasr_log(
+        f"Created input3: {remote_file} with content: {CreateInput.INPUT_3_CONTENT.value}"
     )
--- functions/02b_test_py_api.py
+++ functions/02b_test_py_api.py
@@ -1,6 +1,7 @@
 from FaaSr_py.client.py_client_stubs import (
     faasr_delete_file,
     faasr_get_file,
+    faasr_log,
     faasr_put_file,
 )
 
@@ -17,30 +18,42 @@ def test_py_api(
     output2: str,
 ) -> None:
     invocation_id = get_invocation_id()
+    faasr_log(f"Using invocation ID: {invocation_id}")
 
     # Test deleting input1
     faasr_delete_file(f"{invocation_id}/{input1}")
+    faasr_log(f"Deleted input1: {input1}")
 
     # Test getting input2
+    remote_file = f"{invocation_id}/{input2}"
     faasr_get_file(
         local_file=input2,
-        remote_file=f"{invocation_id}/{input2}",
+        remote_file=remote_file,
         remote_folder=folder,
     )
+    faasr_log(f"Saved remote file: {remote_file} to {input2}")
 
     # Test getting input3
+    remote_file = f"{invocation_id}/{input3}"
     faasr_get_file(
         local_file=input3,
-        remote_file=f"{invocation_id}/{input3}",
+        remote_file=remote_file,
         remote_folder=folder,
     )
+    faasr_log(f"Saved remote file: {remote_file} to {input3}")
 
     # Test putting output1
     with open(output1, "w") as f:
         f.write(TestPyApi.OUTPUT_1_CONTENT.value)
     faasr_put_file(local_file=output1, remote_file=output1, remote_folder=folder)
+    faasr_log(
+        f"Saved remote file: {output1} to {output1} with content: {TestPyApi.OUTPUT_1_CONTENT.value}"
+    )
 
     # Test putting output2
     with open(output2, "w") as f:
         f.write(TestPyApi.OUTPUT_2_CONTENT.value)
     faasr_put_file(local_file=output2, remote_file=output2, remote_folder=folder)
+    faasr_log(
+        f"Saved remote file: {output2} to {output2} with content: {TestPyApi.OUTPUT_2_CONTENT.value}"
+    )
