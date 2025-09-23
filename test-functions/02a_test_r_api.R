30cee9cd246962dee34a00f25e99d5ed74199d61
Laura Kuo <ckuo28@wisc.edu>
2025-09-23 14:23:45 -0700

    update log messages

--- test-functions/02a_test_r_api.R
+++ test-functions/02a_test_r_api.R
@@ -46,9 +46,13 @@ test_r_api <- function(folder, input4, input2, input3, output1, output2) {
   # Test putting output1
   writeLines("Test output1", output1)
   faasr_put_file(local_file=output1, remote_folder=folder, remote_file=output1)
+  msg = paste0("Saved local file: ", output1, " to ", output1, " with content: Test output1")
+  faasr_log(msg)
   
   # Test putting output2
   writeLines("Test output2", output2)
   faasr_put_file(local_file=output2, remote_folder=folder, remote_file=output2)
-  
+  msg = paste0("Saved local file: ", output2, " to ", output2, " with content: Test output2")
+  faasr_log(msg)
+
 }
