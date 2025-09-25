08a1698610b5f74b622f2cb9463acc349a7723b2
Jesse Stover <j.stover.r@gmail.com>
2025-09-25 13:27:11 -0500

    Functions for testing ranked and not ranked

---
+++ functions/08b_test_ranked.py
@@ -0,0 +1,9 @@
+from FaaSr_py.client.py_client_stubs import faasr_log, faasr_rank
+
+
+def test_ranked() -> None:
+    rank_info = faasr_rank()
+
+    faasr_log(f"Rank info: {rank_info}")
+    faasr_log(f"Rank: {rank_info.get('rank')}")
+    faasr_log(f"Max rank: {rank_info.get('max_rank')}")
