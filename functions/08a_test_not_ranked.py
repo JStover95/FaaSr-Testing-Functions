8b2de35f1ac5f8eafd0fb80844536ba51b0e0a42
Jesse Stover <j.stover.r@gmail.com>
2025-09-25 14:32:55 -0500

    Ranked test functions

--- functions/08a_test_not_ranked.py
+++ functions/08a_test_not_ranked.py
@@ -1,9 +1,24 @@
-from FaaSr_py.client.py_client_stubs import faasr_log, faasr_rank
+import os.path
+
+from FaaSr_py.client.py_client_stubs import faasr_log, faasr_put_file, faasr_rank
+from utils import get_invocation_id
 
 
-def test_not_ranked() -> None:
+def test_not_ranked(folder: str) -> None:
     rank_info = faasr_rank()
+    invocation_id = get_invocation_id()
 
-    faasr_log(f"Rank info: {rank_info}")
-    faasr_log(f"Rank: {rank_info.get('rank')}")
-    faasr_log(f"Max rank: {rank_info.get('max_rank')}")
+    local_file = "test_not_ranked.txt"
+    remote_file = os.path.join(invocation_id, local_file)
+
+    with open(local_file, "w") as f:
+        f.write(f"Rank: {rank_info.get('rank')}")
+        f.write(f"Max rank: {rank_info.get('max_rank')}")
+
+    faasr_put_file(
+        local_file=local_file,
+        remote_folder=folder,
+        remote_file=remote_file,
+    )
+
+    faasr_log(f"Saved remote file: {os.path.join(folder, remote_file)}")
