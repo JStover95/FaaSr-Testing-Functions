f14f0f2fa70755b58d865d3e6a6dd9bf468c27b4
Laura Kuo <ckuo28@wisc.edu>
2025-10-01 11:46:48 -0700

    update test-r-api

--- test-functions/02a_test_r_api.R
+++ test-functions/02a_test_r_api.R
@@ -44,14 +44,14 @@ test_r_api <- function(folder, input4, input2, input3, output1, output2) {
   arrow_input3 <- arrow::read_csv_arrow(remote_path3)
   
   # Test putting output1
-  writeLines("Test output1", output1)
+  cat("Test output1", file = output1, sep = "")
   remote_file <- paste0(invocation_id, '/', output1)
   faasr_put_file(local_file=output1, remote_folder=folder, remote_file=remote_file)
   msg = paste0("Created output file: ", remote_file, " with content: Test output1")
   faasr_log(msg)
   
   # Test putting output2
-  writeLines("Test output2", output2)
+  cat("Test output2", file = output2, sep = "")
   remote_file <- paste0(invocation_id, '/', output2)
   faasr_put_file(local_file=output2, remote_folder=folder, remote_file=remote_file)
   msg = paste0("Created output file: ", remote_file, " with content: Test output2")
